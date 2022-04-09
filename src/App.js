0f1d9d2f98f8d136236072faebbbc6d83fbdc802
Jenniffer Tenempaguay <jenntenem@gmail.com>
2022-04-09 17:54:35 -0500

    Agregar las rutas específicas para cada página renderizada

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+//npm install react-router-dom@6
+
 import Cards from './Components/Cards'
+import Form from './Components/Form/Form'
 
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <Cards />
+      <Router>
+        <Routes>
+          <Route exact path="/" >
+            <Route index element={<Cards />} />
+            <Route 
+              path="form"
+              element={<Form className='container'/>}
+            />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   )
 }
