e09db10fd86be4f951615bd1c7dd3c03e6938f6e
Jenniffer Tenempaguay <jenntenem@gmail.com>
2022-04-10 15:09:58 -0500

    Agregar validaciones del formulario con librería react-hook-form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
-
+import { useForm } from 'react-hook-form'
+// npm install react-hook-form
 import List from './DataFormList'
 
 export default function Form() {
@@ -9,6 +10,8 @@ export default function Form() {
       lastname: 'lastname'
     })
   const [list, setList] = useState([])
+  // const { register, handleSubmit, errors } = useForm() 
+  const { register, handleSubmit, formState:{errors} } = useForm() // initialise the hook form
 
   const onChange = (e) => {
     setDatos({
@@ -29,7 +32,8 @@ export default function Form() {
   return (
     <React.Fragment>
       <h1>Form</h1>
-      <form className='row justify-content-center' onSubmit={onSubmit}>
+      {/* <form className='row justify-content-center' onSubmit={onSubmit}> -> Without hook-form */}
+      <form className='row justify-content-center' onSubmit={handleSubmit(onSubmit)}>
         <div className='col-md-3'>
           <input 
             name='name'
@@ -38,6 +42,20 @@ export default function Form() {
             type='text'
             onChange={onChange}
             value={datos.nombre}
+            autoComplete="off"
+            // Referencia de la versión Hook Form 6.X.X
+            // ref={register({ 
+            //   required: {value: true, message: 'El nombre es requerido'},
+            //   minLength: {value: 3, message: 'El nombre debe tener al menos 3 caracteres'},
+            // })}
+
+            // Referencia de la versión Hook Form 7.X.X
+            {
+              ...register('name', {
+                required: {value: true, message: 'El nombre es requerido'},
+                minLength: {value: 3, message: 'El nombre debe tener al menos 3 caracteres'},
+              })
+            }
           />
         </div>
         <div className='col-md-3'>
@@ -48,6 +66,12 @@ export default function Form() {
             type='text'
             onChange={onChange}
             value={datos.apellido}
+            {
+              ...register('lastname', {
+                required: {value: true, message: 'El apellido es requerido'},
+                minLength: {value: 4, message: 'El apellido debe tener al menos 4 caracteres'},
+              })
+            }
           />
         </div>
         <div className='col-md-2'>
@@ -59,6 +83,9 @@ export default function Form() {
           </button>
         </div>
       </form>
+      <span className="text-danger text-small d-block mb-2">
+        {errors?.name && errors?.name?.message} <br /> {errors?.lastname && errors?.lastname?.message}
+      </span>
       <section>
         <h3>{datos.name +" - "+ datos.lastname}</h3>
       </section>
